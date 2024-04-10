e3765e26ebe5b03f1e622e393390abd0b20160a5
AryanGupta1211 <124623548+AryanGupta1211@users.noreply.github.com>
2024-04-10 17:46:14 +0530

    Main file

---
+++ gemeniBot.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import google.generativeai as genai
+
+st.title("Aryan's Chatbot")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    # Configure Google Gemini API
+    genai.configure(api_key='AIzaSyDADS7dZVhmP4la18EfnzcyyY5miONkxFE')
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response.text)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response.text})
